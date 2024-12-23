56e9976295ce2d24e035241537acc7ff6cc92b5c
Zergity <zergity@gmail.com>
2024-12-23 17:11:10 +0700

    cleaning test

--- test/sdk.test.ts
+++ test/sdk.test.ts
@@ -71,7 +71,7 @@ describe('SDK', () => {
     const swapper = sdk.createSwapper(rpcUrl)
     // NATIVE - A
     {
-      const { amountOuts, gasUsed  } = await swapper.simulate({
+      const { amountOuts, gasUsed } = await swapper.simulate({
         tokenIn: NATIVE_ADDRESS,
         tokenOut: packPosId(poolToSwap, POOL_IDS.A),
         amount: numberToWei(0.0001, 18),
@@ -80,7 +80,7 @@ describe('SDK', () => {
           pools
         }
       })
-      const amountOut = amountOuts[amountOuts.length-1]
+      const amountOut = amountOuts[amountOuts.length - 1]
       expect(amountOut.toString()).toEqual('51625135')
       expect(gasUsed).toEqual(2392715)
     }
@@ -94,7 +94,7 @@ describe('SDK', () => {
           pools
         }
       })
-      const amountOut = amountOuts[amountOuts.length-1]
+      const amountOut = amountOuts[amountOuts.length - 1]
       expect(amountOut.toString()).toEqual('23012510')
       expect(gasUsed).toEqual(2393040)
     }
@@ -108,7 +108,7 @@ describe('SDK', () => {
           pools
         }
       })
-      const amountOut = amountOuts[amountOuts.length-1]
+      const amountOut = amountOuts[amountOuts.length - 1]
       expect(amountOut.toString()).toEqual('218198033')
       expect(gasUsed).toEqual(2410961)
     }
@@ -135,9 +135,9 @@ describe('SDK', () => {
     account.processLogs(txLogs)
     const swapper = sdk.createSwapper(rpcUrl)
     // Token R -> A
-    const poolToSwapR = pools[poolToSwap].config?.TOKEN_R 
+    const poolToSwapR = pools[poolToSwap].config?.TOKEN_R
     expect(poolToSwapR?.length).toBeGreaterThanOrEqual(42)
-    const { amountOuts: amountROutsA, gasUsed: gasUsedRA} = await swapper.simulate({
+    const { amountOuts: amountROutsA, gasUsed: gasUsedRA } = await swapper.simulate({
       tokenIn: poolToSwapR || '',
       tokenOut: packPosId(poolToSwap, POOL_IDS.A),
       amount: numberToWei(0.0001, 18),
@@ -149,7 +149,7 @@ describe('SDK', () => {
     expect(Number(amountROutsA)).toBeGreaterThan(0)
     expect(Number(gasUsedRA)).toBeGreaterThan(0)
     // Token R -> B
-    const { amountOuts: amountROutsB, gasUsed: gasUsedRB} = await swapper.simulate({
+    const { amountOuts: amountROutsB, gasUsed: gasUsedRB } = await swapper.simulate({
       tokenIn: poolToSwapR || '',
       tokenOut: packPosId(poolToSwap, POOL_IDS.B),
       amount: numberToWei(0.0001, 18),
@@ -161,7 +161,7 @@ describe('SDK', () => {
     expect(Number(amountROutsB)).toBeGreaterThan(0)
     expect(Number(gasUsedRB)).toBeGreaterThan(0)
     // Token R -> C
-    const { amountOuts: amountROutsC, gasUsed: gasUsedRC} = await swapper.simulate({
+    const { amountOuts: amountROutsC, gasUsed: gasUsedRC } = await swapper.simulate({
       tokenIn: poolToSwapR || '',
       tokenOut: packPosId(poolToSwap, POOL_IDS.C),
       amount: numberToWei(0.0001, 18),
@@ -194,39 +194,48 @@ describe('SDK', () => {
     const account = sdk.createAccount(accountAddress)
     account.processLogs(txLogs)
     const swapper = sdk.createSwapper(rpcUrl)
-    const { amountOuts:amountOutsUSDCA, gasUsed: gasUsedUSDCA  } = await swapper.simulate({
-      tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
-      tokenOut: packPosId(poolToSwap, POOL_IDS.A),
-      amount: "100000",
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsUSDCA)).toBeGreaterThan(0)
-    expect(Number(gasUsedUSDCA)).toBeGreaterThan(0)
-    const { amountOuts:amountOutsUSDCB, gasUsed: gasUsedUSDCB  } = await swapper.simulate({
-      tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
-      tokenOut: packPosId(poolToSwap, POOL_IDS.B),
-      amount: "100000",
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsUSDCB)).toBeGreaterThan(0)
-    expect(Number(gasUsedUSDCB)).toBeGreaterThan(0)
-    const { amountOuts:amountOutsUSDCC, gasUsed:gasUsedUSDCC  } = await swapper.simulate({
-      tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
-      tokenOut: packPosId(poolToSwap, POOL_IDS.C),
-      amount: "100000",
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsUSDCC)).toBeGreaterThan(0)
-    expect(Number(gasUsedUSDCC)).toBeGreaterThan(0)
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
+        tokenOut: packPosId(poolToSwap, POOL_IDS.A),
+        amount: "100000",
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('15')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
+        tokenOut: packPosId(poolToSwap, POOL_IDS.B),
+        amount: "100000",
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('6')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
+        tokenOut: packPosId(poolToSwap, POOL_IDS.C),
+        amount: "100000",
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(10000000).toString()).toEqual('6')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
   })
 
   test('positions-swap', async () => {
@@ -249,39 +258,48 @@ describe('SDK', () => {
     const positionPoolARB = '0xf3cE4cbfF83AE70e9F76b22cd9b683F167d396dd' // Derion pool ARB/ETH
     const positionPoolWBTC = '0x3ed9997b3039b4A000f1BAfF3F6104FB05F4e53B' // Derion pool WBTC/USDC
     const swapper = sdk.createSwapper(rpcUrl)
-    const {amountOuts:amountOutsAC, gasUsed: gasUsedAC} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
-      tokenOut: packPosId(positionPoolWBTC, POOL_IDS.C),
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsAC)).toBeGreaterThan(0)
-    expect(Number(gasUsedAC)).toBeGreaterThan(0)
-    const {amountOuts:amountOutsAB, gasUsed: gasUsedAB} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
-      tokenOut: packPosId(positionPoolARB, POOL_IDS.B),
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsAB)).toBeGreaterThan(0)
-    expect(Number(gasUsedAB)).toBeGreaterThan(0)
-    const {amountOuts:amountOutsBC, gasUsed: gasUsedBC} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
-      tokenOut: packPosId(positionPoolWBTC, POOL_IDS.C),
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsBC)).toBeGreaterThan(0)
-    expect(Number(gasUsedBC)).toBeGreaterThan(0)
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
+        tokenOut: packPosId(positionPoolWBTC, POOL_IDS.C),
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('2666087772')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
+        tokenOut: packPosId(positionPoolARB, POOL_IDS.B),
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('647')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
+        tokenOut: packPosId(positionPoolWBTC, POOL_IDS.C),
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('1532955386')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
   })
 
   test('positions-close', async () => {
@@ -306,128 +324,155 @@ describe('SDK', () => {
     const positionPoolWETH = '0xAaf8FAC8F5709B0c954c9Af1d369A9b157e31FfE' // Derion pool WBTC/USDC
 
     const swapper = sdk.createSwapper(rpcUrl)
-    console.log('A -> NATIVE')
-    const {amountOuts:amountOutsANative, gasUsed: gasUsedANative} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
-      tokenOut: pools[positionPoolARB].config?.TOKEN_R || "",
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsANative)).toBeGreaterThan(0)
-    expect(Number(gasUsedANative)).toBeGreaterThan(0)
-    console.log('B -> NATIVE')
+    {
+      console.log('A -> NATIVE')
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
+        tokenOut: pools[positionPoolARB].config?.TOKEN_R || "",
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('2807582157')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('B -> NATIVE')
 
-    const {amountOuts:amountOutsBNative, gasUsed: gasUsedBNative} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
-      tokenOut: NATIVE_ADDRESS,
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
+        tokenOut: NATIVE_ADDRESS,
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
 
-    expect(Number(amountOutsBNative)).toBeGreaterThan(0)
-    expect(Number(gasUsedBNative)).toBeGreaterThan(0)
-    console.log('C -> NATIVE')
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('1626512847')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('C -> NATIVE')
 
-    const {amountOuts:amountOutsCNative, gasUsed: gasUsedCNative} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
-      tokenOut: NATIVE_ADDRESS,
-      amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsCNative)).toBeGreaterThan(0)
-    expect(Number(gasUsedCNative)).toBeGreaterThan(0)
-
-
-    console.log('A -> R')
-    const {amountOuts:amountOutsAR, gasUsed: gasUsedAR} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
-      tokenOut: pools[positionPoolARB].config?.TOKEN_R || "",
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsAR)).toBeGreaterThan(0)
-    expect(Number(gasUsedAR)).toBeGreaterThan(0)
-    console.log('B -> R')
-
-    const {amountOuts:amountOutsBR, gasUsed: gasUsedBR} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
-      tokenOut:  pools[positionPoolARB].config?.TOKEN_R || "",
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-
-    expect(Number(amountOutsBR)).toBeGreaterThan(0)
-    expect(Number(gasUsedBR)).toBeGreaterThan(0)
-    console.log('C -> R')
-
-    const {amountOuts:amountOutsCR, gasUsed: gasUsedCR} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
-      tokenOut:  pools[positionPoolWETH].config?.TOKEN_R || "",
-      amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsCR)).toBeGreaterThan(0)
-    expect(Number(gasUsedCR)).toBeGreaterThan(0)
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
+        tokenOut: NATIVE_ADDRESS,
+        amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('421426528')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
 
 
-    console.log('A -> USDC')
+    {
+      console.log('A -> R')
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
+        tokenOut: pools[positionPoolARB].config?.TOKEN_R || "",
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('2807582157')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('B -> R')
+
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
+        tokenOut: pools[positionPoolARB].config?.TOKEN_R || "",
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('1626507164')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('C -> R')
+
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
+        tokenOut: pools[positionPoolWETH].config?.TOKEN_R || "",
+        amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(1000000).toString()).toEqual('421426805')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+
     const USDC = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
-    const {amountOuts:amountOutsAUSDC, gasUsed: gasUsedAUSDC} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
-      tokenOut: USDC,
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsAUSDC)).toBeGreaterThan(0)
-    expect(Number(gasUsedAUSDC)).toBeGreaterThan(0)
-    console.log('B -> USDC')
 
-    const {amountOuts:amountOutsBUSDC, gasUsed: gasUsedBUSDC} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
-      tokenOut: USDC,
-      amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
+    {
+      console.log('A -> USDC')
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.A),
+        tokenOut: USDC,
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.A)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(100).toString()).toEqual('93514')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('B -> USDC')
 
-    expect(Number(amountOutsBUSDC)).toBeGreaterThan(0)
-    expect(Number(gasUsedBUSDC)).toBeGreaterThan(0)
-    console.log('C -> USDC')
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolARB, POOL_IDS.B),
+        tokenOut: USDC,
+        amount: account.positions[packPosId(positionPoolARB, POOL_IDS.B)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
 
-    const {amountOuts:amountOutsCUSDC, gasUsed: gasUsedCUSDC} = await swapper.simulate({
-      tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
-      tokenOut: USDC,
-      amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
-      deps: {
-        signer,
-        pools
-      }
-    })
-    expect(Number(amountOutsCUSDC)).toBeGreaterThan(0)
-    expect(Number(gasUsedCUSDC)).toBeGreaterThan(0)
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(100).toString()).toEqual('54206')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
+    {
+      console.log('C -> USDC')
+
+      const { amountOuts, gasUsed } = await swapper.simulate({
+        tokenIn: packPosId(positionPoolWETH, POOL_IDS.C),
+        tokenOut: USDC,
+        amount: account.positions[packPosId(positionPoolWETH, POOL_IDS.C)].balance.toString(),
+        deps: {
+          signer,
+          pools
+        }
+      })
+      const amountOut = amountOuts[amountOuts.length - 1]
+      expect(amountOut.div(100).toString()).toEqual('14045')
+      expect(gasUsed).toBeLessThan(3000000)
+    }
 
   })
 })
